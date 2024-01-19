a9857f3e2325f5e718db58702903132a55b018d0
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-01-19 12:14:15 +0900

    Update jinja2_tpl_template.py

--- 6.jinja2_tpl_template/jinja2_tpl_template.py
+++ 6.jinja2_tpl_template/jinja2_tpl_template.py
@@ -1,25 +1,31 @@
-# from netmiko import ConnectHandler
+
+
 from jinja2 import Environment, FileSystemLoader
 
-sw1 = {
-        'device_type': 'cisco_ios',
-        'ip': '192.168.2.11',
-        'username': 'python',
-        'password': '123'
+param_dict = {
+    "params": {
+        "commands": [
+            {
+                "command_name": "system_license_delete",
+                "interface": "111",
+                "vlan_id": "222",
+
+            },
+        ]
+    }
 }
+
+def reset_info(param_dict, key_list):
+    return [param_dict[key] for key in key_list]
+
+
+
 
 loader = FileSystemLoader('templates')
 environment = Environment(loader=loader)
+environment.filters['reset_info'] = reset_info
+
 tpl = environment.get_template('acl.conf.tpl')
 
-allow_ip = ['10.1.1.1', '10.1.1.2']
-disallow_ip = ['10.1.1.3', '10.1.1.4']
-out = tpl.render(allow_ip=allow_ip, disallow_ip=disallow_ip, interface='loopback 1')
+out = tpl.render(param_dict)
 print(out)
-# with open("configuration.conf", "w") as f:
-#        f.write(out)
-#
-# with ConnectHandler(**sw1) as conn:
-#         print ("已经成功登陆交换机" + sw1['ip'])
-#         output = conn.send_config_from_file("configuration.conf")
-#         print (output)
