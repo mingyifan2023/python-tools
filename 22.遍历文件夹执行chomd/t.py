d5a1139de365af9d0e29339193430881b72041fb
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-04-03 09:28:18 +0900

    Update t.py

--- 22.遍历文件夹执行chomd/t.py
+++ 22.遍历文件夹执行chomd/t.py
@@ -1,13 +1,15 @@
 import os
 
 def set_permissions(directory):
-    # 设置最上层文件夹权限
+    # 设置当前文件夹权限
     os.chmod(directory, 0o777)
-    
-    # 遍历文件夹并设置权限
+
+    # 遍历文件夹中的所有文件并设置权限
     for root, dirs, files in os.walk(directory):
         for d in dirs:
             os.chmod(os.path.join(root, d), 0o777)
+        for f in files:
+            os.chmod(os.path.join(root, f), 0o777)
 
 # 获取当前工作目录
 directory = os.getcwd()
